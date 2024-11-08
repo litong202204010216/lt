5bfd0ca9548f03dd2dbf7d61c8e6e5ee6f270b90
lt16 <2868903504@qq.com>
2024-11-08 11:59:05 +0800

    第一次提交

---
+++ book/forms.py
@@ -0,0 +1,22 @@
+
+
+from django.forms import ModelForm, Textarea
+from .models import Review
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+class ReviewForm(ModelForm) :
+    def __init__(self, *args, **kwargs) :
+        super(ModelForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({'class':'form-control'})
+        self.fields['watchAgain'].widget.attrs.update({'class':'form-check-input'})
+
+    class Meta:
+        model = Review
+        fields = ['text', 'watchAgain']
+        labels = {
+            'text':('说说你看过之后的感受吧...'),
+            'watchAgain':('是否会再次观看'),
+        }
+        widgets = {
+            'text':Textarea(attrs={'rows':4}),
+        }
+
